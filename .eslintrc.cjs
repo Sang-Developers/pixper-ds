405e5e284919b08a7fd24046b7daf77d6ccd8701
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-04-21 00:40:31 +0700

    feat: add storybook and re-pretty folder project structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "airbnb/hooks",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended"
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
